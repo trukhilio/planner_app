2e5e6135b2144ad60ad0ec4555e44bf81a356573
trukhilio <trukhantech@gmail.com>
2017-09-07 15:53:31 +0300

    deploying

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -9,9 +9,9 @@ module.exports = {
         './src/index'
     ],
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'static'),
         filename: 'bundle.js',
-        publicPath: '/dist/'
+        publicPath: '/static/'
     },
     plugins: [
         new webpack.optimize.DedupePlugin(),
